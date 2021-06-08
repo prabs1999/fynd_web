85f258985ae3c0b5e81b3895a1dcd6066ae90bad
prabs1999 <67868742+prabs1999@users.noreply.github.com>
2021-06-08 10:39:24 +0530

    Add files via upload

---
+++ src/app/_models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    phonenumber: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { UserService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users = [];
+    username: string;
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.username= 'prabhat';
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.loadAllUsers());
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+    onusernamechange(value){
+        debugger;
+        console.log(value);
+        console.log(this.username);
+    }
+}
